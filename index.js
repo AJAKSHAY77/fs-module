f9c8a5310b1e32916289ae8e89939cd7a623cc9e
akshay jain <123448064+AJAKSHAY77@users.noreply.github.com>
2023-07-18 17:38:47 +0530

    Update fs modules index.js
    reading,write ,append,delete, are some methods in fs module

--- index.js
+++ index.js
@@ -1,20 +1,103 @@
 const file = require("fs")
 
-console.log("start reading");
+// console.log("start reading");
+
+// file.readFile("input.txt",function(err,data){
+//     if(err){
+//        const  err = "invalid input"
+//         console.log("error",err);
+//         return err
+//     }
+//      console.log("data",data.toString());
+//      return data
+    
+
+// })
+// console.log("other stuff");
+
+// const readfile = file.readFileSync("input.txt")
+// console.log(readfile.toString());
+// console.log("other staff");
+
+
+// if you to write in the file then we use write method
+
+// file.writeFile("input.txt"," bhaia",function(err){
+//           if(err){
+//             console.log("erro in the file",err);
+//             return err
+
+//           }else{
+//            console.log("success in writing file");
+          
+//           }
+
+          
+// })
+
+//apending file or for example in above method it is overinding the whole data 
+// but i want to add some text but not deleting the existing element
+
+
+// apend to file
+
+// file.appendFile("input.txt","__akshay_bhaiya","utf-8",function(err){
+//     if(err){
+//         console.log("erron in adding file",err);
+//         return err
+
+//         }else{
+//             console.log("sucessfully addtext text to your file ");
+//         }
+
+// })
+
+//close file
+
+// deleting file
+// file.unlink("input.txt",function(err){
+//     if(err){
+//         console.log("erron in deleting file",err);
+//         return err
+
+//         }else{
+//             console.log("sucessfully deleted your file ");
+//         }
+// })
 
 file.readFile("input.txt",function(err,data){
     if(err){
-       const  err = "invalid input"
-        console.log("error",err);
+        console.log("erron in reading file",err);
         return err
-    }
-     console.log("data",data.toString());
-     return data
-    
+
+        }else{
+            console.log("sucessfully readed file ",data.toString());
+            return data
+        }
+})
+
+
+file.writeFile("input.txt"," bhaia",function(err){
+          if(err){
+            console.log("erro in the file",err);
+            return err
+
+          }else{
+           console.log("success in writing file");
+          
+          }
+
+          
+})
+file.unlink("input.txt",function(err){
+
+    if(err){
+        console.log("erro in the file",err);
+        return err
+
+      }else{
+       console.log("success in writing file");
+      
+      }
 
 })
-console.log("other stuff");
-
-const readfile = file.readFileSync("input.txt")
-console.log(readfile.toString());
-console.log("other staff");
